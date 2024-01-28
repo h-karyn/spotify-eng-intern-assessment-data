61f85ef4b0cc98ccee8efc46ae33fa438f7b745e
h-karyn <muhua.huang@foxmail.com>
2024-01-28 15:09:49 -0600

    completed q3

--- tests/test_sql_queries.py
+++ tests/test_sql_queries.py
@@ -22,7 +22,6 @@ class TestSQLQueries(unittest.TestCase):
 
         # Load data from CSV files into the database
         for csv_file, table_name in csv_files_tables.items():
-            # print(f"Loading {csv_file} into {table_name}")
             load_csv_to_mysql(csv_file, table_name, self.cur)
 
         self.conn.commit()
@@ -36,7 +35,7 @@ class TestSQLQueries(unittest.TestCase):
         # Open task1.sql and read all queries
         with open('../sql/task1.sql', 'r') as file:
             sql_queries = file.read().split(
-                ';')  # Assuming queries are separated by ';'
+                ';')
 
         # Remove any empty queries after splitting
         sql_queries = [query.strip() for query in sql_queries if query.strip()]
@@ -123,8 +122,7 @@ class TestSQLQueries(unittest.TestCase):
 
     def test_task2(self):
         with open('../sql/task2.sql', 'r') as file:
-            sql_queries = file.read().split(
-                ';')  # Assuming queries are separated by ';'
+            sql_queries = file.read().split(';')
 
         # Remove any empty queries after splitting
         sql_queries = [query.strip() for query in sql_queries if query.strip()]
@@ -155,9 +153,47 @@ class TestSQLQueries(unittest.TestCase):
                 self.cur.execute(query)
                 result = self.cur.fetchall()
                 self.assertEqual(result, expected_results[i],
-                                 f"Query {i + 5} in task1.sql output doesn't match expected result.")
+                                 f"Query {i + 5} in task2.sql output doesn't match expected result.")
 
     # Add more test methods for additional SQL tasks
+    def test_task3(self):
+        with open('../sql/task3.sql', 'r') as file:
+            sql_queries = file.read().split(';')
+
+        # Remove any empty queries after splitting
+        sql_queries = [query.strip() for query in sql_queries if query.strip()]
+
+        expected_result_9 = [(8, 'Sports & Outdoors', 155.00),
+                             (4, 'Home & Kitchen', 145.00),
+                             (1, 'Electronics', 125.00)]
+
+        expected_result_10 = [(5, 'sarahwilson')]
+
+        expected_result_11 = [(1, 'Smartphone X', 1, 500.00),
+                              (3, 'Laptop Pro', 2, 1200.00),
+                              (6, 'Designer Dress', 3, 300.00),
+                              (7, 'Coffee Maker', 4, 80.00),
+                              (9, 'Action Camera', 5, 200.00),
+                              (12, 'Skincare Set', 6, 150.00),
+                              (14, 'Weighted Blanket', 7, 100.00),
+                              (15, 'Mountain Bike', 8, 1000.00)]
+
+        expected_result_12 = []
+
+        expected_results = [
+            expected_result_9,
+            expected_result_10,
+            expected_result_11,
+            expected_result_12
+        ]
+
+        # Execute each query and compare the result with the expected outcome
+        for i, query in enumerate(sql_queries):
+            with self.subTest(query=i):
+                self.cur.execute(query)
+                result = self.cur.fetchall()
+                self.assertEqual(result, expected_results[i],
+                                 f"Query {i + 9} in task3.sql output doesn't match expected result.")
 
 
 if __name__ == '__main__':
